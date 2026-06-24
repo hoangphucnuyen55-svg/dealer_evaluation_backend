bb6812c553d1c7e4654a352cb4fdd1b7ef8055b5
hoangphucnuyen55-svg <hoangphucnuyen55@gmail.com>
2026-06-24 08:19:15 +0700

    Create Register.jsx

---
+++ server/frontend/src/components/Register/Register.jsx
@@ -0,0 +1,35 @@
+<form>
+  <input
+    type="text"
+    name="username"
+    placeholder="Username"
+  />
+
+<input
+ type="text"
+ name="first_name"
+ placeholder="First Name"
+/>
+
+<input
+ type="text"
+ name="last_name"
+ placeholder="Last Name"
+/>
+
+<input
+ type="email"
+ name="email"
+ placeholder="Email"
+/>
+
+<input
+ type="password"
+ name="password"
+ placeholder="Password"
+/>
+
+  <button type="submit">
+    Register
+  </button>
+</form>
